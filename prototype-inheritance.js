83d7bcc14cf3eed30fd55a4d17f665295887bbf0
tim <timon0596@yandex.ru>
2020-12-13 12:31:21 +0300

    polyfill and prototype inheritance

---
+++ prototype-inheritance.js
@@ -0,0 +1,33 @@
+Object.create = function(o) {
+    function F() {}
+    F.prototype = o;
+    return new F();
+  };
+
+
+function A (a){
+  this.a=a
+}
+
+
+A.prototype.getProduct = function(){
+  return this.a**2
+}
+
+
+function B (a,b){
+  A.call(this,a)
+  this.b=b
+}
+
+
+B.prototype = Object.create(A.prototype)
+
+B.prototype.getProduct = function(){
+  return A.prototype.getProduct.call(this)*this.b
+}
+
+let a = new A(2)
+let b = new B(2,2)
+console.log(b.getProduct())//8
+console.log(a.getProduct())//4
