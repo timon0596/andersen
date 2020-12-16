024eee12896714555b5b4af450c0c4ff3a462996
tim <timon0596@yandex.ru>
2020-12-16 16:32:50 +0300

    practical task

--- src/index.js
+++ src/index.js
@@ -1,98 +1,86 @@
-import './main.css';
-
-function card({ props = {}, i }) {
-  const el = document.createElement('div');
-  el.classList.add('card');
-  el.setAttribute('data-open', String(i));
-  el.innerHTML = `<h2 class="card__name">
-        ${props.name}
-      </h2>
-      <div class="card__body">
-        <div class="card__heading">
-          <h3>Тип:</h3>
-          <div class="card__text">
-            ${props.type}
-          </div>
-        </div>
-        <div class="card__heading">
-          <h3>Класс:</h3>
-          <div class="card__text">
-            ${props.class}
-          </div>
-        </div>
-        <div class="card__heading">
-          <h3>Отряд:</h3>
-          <div class="card__text">
-            ${props.squad}
-          </div>
-        </div>
-      </div>`;
-  return el;
+function promiseRace(promisesArray) {
+  let counter = promisesArray.length;
+  const errors = [];
+  return new Promise((resolve, reject) => {
+    promisesArray.forEach((promise) => {
+      Promise.resolve(promise)
+        .then(resolve)
+        .catch((er) => {
+          errors.push(er);
+          counter--;
+          if (!counter) {
+            reject(errors);
+          }
+        });
+    });
+  });
 }
 
-function modal({ props = {}, i }) {
-  const el = document.createElement('div');
-  el.style.display = 'none';
-  el.classList.add('modal');
-  el.setAttribute('data-open', String(i));
-  el.innerHTML = `
-    <div class="modal__mask"></div>
-    <div class="modal__body">
-      <div class="modal__header">
-        <h2 class="modal__name">${props.name}</h2>
-        <div class="modal__close">&times;</div>
-      </div>
-      <div class="modal__description">${props.description}</div>
-    </div>`;
-  return el;
-}
-const body = document.querySelector('body');
-const root = document.querySelector('.root');
-const descriptions = [
-  {
-    name: 'лев',
-    description: 'Наряду с тигром — самая крупная из ныне живущих кошек, масса некоторых самцов может достигать 250 кг. Трудно сказать достоверно, массивнее ли крупнейшие подвиды льва, чем крупнейшие подвиды тигров.',
-  },
-  {
-    name: 'Китовая акула',
-    description: 'Китовая акула, в отличие от большинства других акул, питается преимущественно одним планктоном, отцеживая корм из воды с помощью особого цедильного аппарата, образованного жаберными дугами',
-  },
+const promises = [
+  new Promise((res, rej) => {
+    setTimeout(rej, 3300, 1);
+  }),
+  new Promise((res, rej) => {
+    setTimeout(rej, 3000, 2);
+  }),
+  new Promise((res, rej) => {
+    setTimeout(rej, 3330, 3);
+  }),
 ];
-const animals = [
-  {
-    name: 'лев',
-    type: 'хордовые',
-    class: 'млекопитающие',
-    squad: 'хищные',
-  },
-  {
-    name: 'Китовая акула',
-    type: 'хордовые',
-    class: 'хрящевые рыбы',
-    squad: 'воббегонгообразные',
-  },
-].forEach((props, i) => {
-  const el = card({ props, i });
-  const modalWindow = modal({ props: descriptions[i], i });
-  root.append(el);
-  body.append(modalWindow);
+
+const promises1 = [
+  new Promise((res, rej) => {
+    setTimeout(res, 3300, 1);
+  }),
+  new Promise((res, rej) => {
+    setTimeout(res, 3000, 2);
+  }),
+  new Promise((res, rej) => {
+    setTimeout(res, 3330, 3);
+  }),
+];
+
+promiseRace(promises).then((data) => {
+  console.log(data);
+}).catch((er) => {
+  console.error(er);
 });
 
-const closeButtons = document.querySelectorAll('.modal');
-const cards = document.querySelectorAll('.card');
-closeButtons.forEach((el, i) => {
-  el.addEventListener('click', closeButtonClickHandler);
+promiseRace(promises1).then((data) => {
+  console.log(data);
+}).catch((er) => {
+  console.error(er);
 });
-cards.forEach((el, i) => {
-  el.addEventListener('click', cardClickHandler);
-});
-function closeButtonClickHandler(e) {
-  const buttonCondition = [...e.target.classList].indexOf('modal__close') !== -1;
-  const backgroundCondition = [...e.target.classList].indexOf('modal__mask') !== -1;
-  if (buttonCondition || backgroundCondition) {
-    e.currentTarget.style.display = 'none';
-  }
+
+function fetchPolyfill(url, method = 'GET') {
+  return new Promise((res, rej) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = () => {
+      res(xhr.response);
+    };
+    xhr.onerror = () => {
+      rej(xhr.response);
+    };
+    xhr.send();
+  });
 }
-function cardClickHandler(e) {
-  document.querySelector(`.modal[data-open="${e.currentTarget.getAttribute('data-open')}"]`).style.display = '';
+
+function promiseAll(promisesArray) {
+  const output = new Array(promisesArray.length);
+  let counter = 0;
+
+  return new Promise((resolve, reject) => {
+    promisesArray.forEach((promise, index) => {
+      Promise.resolve(promise)
+        .then((value) => {
+          output[index] = value;
+          counter += 1;
+          if (counter === promisesArray.length) {
+            resolve(output);
+          }
+        })
+        .catch(reject);
+    });
+  });
 }
