e5ec706bdca413f128b47d19e239cb8675944cfa
tim <timon0596@yandex.ru>
2020-12-17 16:06:30 +0300

    FAT-11496

--- src/index.js
+++ src/index.js
@@ -1,98 +1,60 @@
-import './main.css';
-
-function card({ props = {}, i }) {
-  const el = document.createElement('div');
-  el.classList.add('card');
-  el.setAttribute('data-open', String(i));
-  el.innerHTML = `<h2 class="card__name">
-        ${props.name}
-      </h2>
-      <div class="card__body">
-        <div class="card__heading">
-          <h3>Тип:</h3>
-          <div class="card__text">
-            ${props.type}
-          </div>
-        </div>
-        <div class="card__heading">
-          <h3>Класс:</h3>
-          <div class="card__text">
-            ${props.class}
-          </div>
-        </div>
-        <div class="card__heading">
-          <h3>Отряд:</h3>
-          <div class="card__text">
-            ${props.squad}
-          </div>
-        </div>
-      </div>`;
-  return el;
+function sum(...args) {
+  return args.reduce((acc, el) => acc + el);
 }
 
-function modal({ props = {}, i }) {
-  const el = document.createElement('div');
-  el.style.display = 'none';
-  el.classList.add('modal');
-  el.setAttribute('data-open', String(i));
-  el.innerHTML = `
-    <div class="modal__mask"></div>
-    <div class="modal__body">
-      <div class="modal__header">
-        <h2 class="modal__name">${props.name}</h2>
-        <div class="modal__close">&times;</div>
-      </div>
-      <div class="modal__description">${props.description}</div>
-    </div>`;
-  return el;
+function sum2(a) {
+  return function (b) {
+    return a + b;
+  };
 }
-const body = document.querySelector('body');
-const root = document.querySelector('.root');
-const descriptions = [
-  {
-    name: 'лев',
-    description: 'Наряду с тигром — самая крупная из ныне живущих кошек, масса некоторых самцов может достигать 250 кг. Трудно сказать достоверно, массивнее ли крупнейшие подвиды льва, чем крупнейшие подвиды тигров.',
-  },
-  {
-    name: 'Китовая акула',
-    description: 'Китовая акула, в отличие от большинства других акул, питается преимущественно одним планктоном, отцеживая корм из воды с помощью особого цедильного аппарата, образованного жаберными дугами',
-  },
-];
-const animals = [
-  {
-    name: 'лев',
-    type: 'хордовые',
-    class: 'млекопитающие',
-    squad: 'хищные',
-  },
-  {
-    name: 'Китовая акула',
-    type: 'хордовые',
-    class: 'хрящевые рыбы',
-    squad: 'воббегонгообразные',
-  },
-].forEach((props, i) => {
-  const el = card({ props, i });
-  const modalWindow = modal({ props: descriptions[i], i });
-  root.append(el);
-  body.append(modalWindow);
-});
 
-const closeButtons = document.querySelectorAll('.modal');
-const cards = document.querySelectorAll('.card');
-closeButtons.forEach((el, i) => {
-  el.addEventListener('click', closeButtonClickHandler);
-});
-cards.forEach((el, i) => {
-  el.addEventListener('click', cardClickHandler);
-});
-function closeButtonClickHandler(e) {
-  const buttonCondition = [...e.target.classList].indexOf('modal__close') !== -1;
-  const backgroundCondition = [...e.target.classList].indexOf('modal__mask') !== -1;
-  if (buttonCondition || backgroundCondition) {
-    e.currentTarget.style.display = 'none';
+function cur(arg) {
+  const terms = [];
+  terms.push(arg);
+  return function innerFunc(arg2) {
+    if (arguments.length) {
+      terms.push(arg2);
+      return innerFunc;
+    }
+    return terms.reduce((acc, el) => acc + el);
+  };
+}
+console.log(cur(1)(2)(3)(4)(5)());
+
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== 'function') {
+      throw new TypeError('Function.prototype.bind - what '
+        + 'is trying to be bound is not callable');
+    }
+
+    const aArgs = Array.prototype.slice.call(arguments, 1);
+    const fToBind = this;
+    const fNOP = function () {};
+    const fBound = function () {
+      return fToBind.apply(
+        (
+          this instanceof fNOP
+            && oThis ? this : oThis
+        ),
+        aArgs.concat(Array.prototype.slice.call(arguments)),
+      );
+    };
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
+}
+
+function mediana(...args) {
+  const arr = args.flat();
+  arr.sort((a, b) => a - b);
+  if (arr.length % 2 === 0) {
+    return (arr[arr.length / 2 - 1] + arr[arr.length / 2]) / 2;
   }
+  return arr[Math.floor(arr.length / 2)];
 }
-function cardClickHandler(e) {
-  document.querySelector(`.modal[data-open="${e.currentTarget.getAttribute('data-open')}"]`).style.display = '';
-}
+
+console.log(mediana([1, 2, 3, 7, 8, 4, 2]));
+console.log(mediana(1, 2, 3, 7, 5, 8, 4, 2));
